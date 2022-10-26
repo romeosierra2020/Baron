e2c494ccf9804cee5b5d1393d6f83525a0a16993
Romeo Sierra <richard.powell.henshaws@gmail.com>
2022-10-26 17:55:59 +0100

    Initial Commit

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-import Game from './game/Game.js'
-import Conker from './conker/Conker.js'
+
 import Platform from './platform/Platform.js'
-import Application from './application/Application.js';
-const game = new Game();
-const conker = new Conker();
-const app = new Application();
+
 const platform = new Platform();
 
